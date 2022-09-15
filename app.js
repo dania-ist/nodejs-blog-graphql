abedd263299e537a612ddf7687f2a0231b363fae
dania istanboli <dania.ist.mi96@gmail.com>
2022-09-15 11:14:45 +0300

    setup server & graphql

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./graphql/schema");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+// express app
+const app = express();
+
+// Middlewares
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
+mongoose.connect(process.env.MONGO_DB_URL).then(() => {
+  app.listen(PORT, () => {
+    console.log("server is working");
+  });
+});
